d2bead8824b2ace212146fe160448f9591d8f6d9
jiaen-p <jiaen.m.pan@gmail.com>
2020-03-08 00:00:58 +0100

    changed deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t jiaenpan/multi-client:latest -t jiaenpan/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t jiaenpan/multi-server:latest -t jiaenpan/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t jiaenpan/multi-worker:latest -t jiaenpan/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t "$DOCKER_USERNAME"/multi-client:latest -t "$DOCKER_USERNAME"/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t "$DOCKER_USERNAME"/multi-server:latest -t "$DOCKER_USERNAME"/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t "$DOCKER_USERNAME"/multi-worker:latest -t "$DOCKER_USERNAME"/multi-worker:$SHA ./worker/Dockerfile ./worker
 # Built images and tag them twice, with latest and with git head hash, and push both to docker hub
-docker push jiaenpan/multi-client:latest
-docker push jiaenpan/multi-server:latest
-docker push jiaenpan/multi-worker:latest
+docker push "$DOCKER_USERNAME"/multi-client:latest
+docker push "$DOCKER_USERNAME"/multi-server:latest
+docker push "$DOCKER_USERNAME"/multi-worker:latest
 
-docker push jiaenpan/multi-client:$SHA
-docker push jiaenpan/multi-server:$SHA
-docker push jiaenpan/multi-worker:$SHA
+docker push "$DOCKER_USERNAME"/multi-client:$SHA
+docker push "$DOCKER_USERNAME"/multi-server:$SHA
+docker push "$DOCKER_USERNAME"/multi-worker:$SHA
 
 # Force update to the latest SHA to the cluster 
 kubectl apply -f ./k8s
-kubectl set image deployments/multi-client-deployment client=jiaenpan/multi-client:$SHA
-kubectl set image deployments/multi-server-deployment server=jiaenpan/multi-server:$SHA
-kubectl set image deployments/multi-worker-deployment worker=jiaenpan/multi-worker:$SHA
+kubectl set image deployments/multi-client-deployment client="$DOCKER_USERNAME"/multi-client:$SHA
+kubectl set image deployments/multi-server-deployment server="$DOCKER_USERNAME"/multi-server:$SHA
+kubectl set image deployments/multi-worker-deployment worker="$DOCKER_USERNAME"/multi-worker:$SHA
